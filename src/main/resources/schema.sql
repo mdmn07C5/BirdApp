76f063b625377956a8dd662de0be912797842872
mdmn07C5 <malicdan@protonmail.ch>
2024-05-01 18:09:04 -0400

    Adds sequence of queries to schema.sql for connectionutil

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,40 @@
-CREATE TABLE user (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT UNIQUE,
-    password TEXT
+drop table if exists post;
+drop table if exists account;
+
+create table account (
+    account_id int primary key auto_increment,
+    username varchar(20) unique,
+    password varchar(255)
 );
 
-CREATE TABLE message (
-    message_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    posted_by INTEGER,
-    message_text TEXT,
-    time_posted_epoch BIGINT,
-    FOREIGN KEY (posted_by) REFERENCES user(user_id)
+create table post (
+    post_id int primary key auto_increment,
+    posted_by int,
+    post_content varchar(140),
+    time_posted_epoch bigint,
+    foreign key (posted_by) references  account(account_id)
 );
+
+
+insert into account (username, password) values ('ApuApustaja', 'password');
+insert into account (username, password) values ('SpurdoSpadre', 'password');
+
+insert into post (posted_by, post_content, time_posted_epoch) values (1,'test message from Apu',1714600414);
+insert into post (posted_by, post_content, time_posted_epoch) values (2,'test message from Spurdo',1714600416);
+
+
+
+-- SQLite syntax LMAO
+-- CREATE TABLE user (
+--     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     username TEXT UNIQUE,
+--     password TEXT
+-- );
+
+-- CREATE TABLE message (
+--     message_id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     posted_by INTEGER,
+--     message_text TEXT,
+--     time_posted_epoch BIGINT,
+--     FOREIGN KEY (posted_by) REFERENCES user(user_id)
+-- );
